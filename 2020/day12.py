aa26285e29f4186655c6b8925fdfbff6f110bb93
Chris Forseth <christopher.forseth@gmail.com>
2020-12-11 21:59:53 -0600

    Stub out day 12 files - setting up home computer for doing this;

---
+++ 2020/day12.py
@@ -0,0 +1,19 @@
+import io
+import re
+import sys
+import time
+from copy import deepcopy 
+
+import aoc
+
+
+# config
+INPUT_FILE = 'day12.sample.txt';
+REMOVE_LINE_BREAKS = True
+
+# read input
+input = aoc.ImportInput(INPUT_FILE, REMOVE_LINE_BREAKS)
+
+# implementation
+for idx, line in enumerate(input): 
+    print( str(idx) + ': ' + line )
