ab8d8086fe9b45b2df95998365cfbad1a9659bcb
Chris Forseth <christopher.forseth@gmail.com>
2020-12-19 14:58:40 -0600

    I'm not great at learning from wikipedia.  So after about a dozen sheets of paper, I have a working Earley Parse!

--- 2020/day19.py
+++ 2020/day19.py
@@ -58,10 +58,6 @@ for rule_text in rule_texts:
     rule_seq = []
     for branch in branches:
         sequence = [to_sequence(s) for s in branch.strip().split(' ')]
-        if sequence == ['a']:
-            a_rule = ikey
-        elif sequence == ['b']:
-            b_rule = ikey
         rule_seq.append(sequence)
 
     rules[ikey] = rule_seq
@@ -170,13 +166,125 @@ def part01():
 # end part01
 
 
+# rule_key is the thing that we're searching for
+# pre is what this item has to start with
+# post is what this item must end with
+# offset is index this was first predicted/scanned, or original completion step
+def add_to_set(set_ref, rule_key, pre, post, offset):
+    item = [rule_key, pre, post, offset]
+    if item not in set_ref:
+        set_ref.append(item)
+
+
+# rule is finished if it has no next steps
+def rule_finished(rule):
+    return len(rule[2]) == 0
+
+
+def is_terminal(item):
+    return item == 'a' or item == 'b'
+
+
+def complete_rule(sets, rule_set, offset):
+    # so for this, any set where my pre item is the same as their first post item
+    # I make a new set, with that rules  key, with this as the pre, and their post being one less
+    set_key = rule_set[0]
+    source_idx = rule_set[3]
+    source_set = sets[source_idx]
+    if source_idx == offset:
+        print('should not happen - if so, I messed up an offset')
+        return
+
+    for rule in source_set:
+        post = rule[2]
+        if (len(post) > 0) and (set_key == post[0]):
+            add_to_set(sets[offset], rule[0], [post[0]], post[1:], rule[3])
+
+
+def scan_rule(sets, rule_set, offset, word):
+    if offset >= len(word):
+        return
+
+    # for every rule I can potentially 'finish',
+    # add it to the next set
+    ch = word[offset]
+    next_rule_set = sets[offset + 1]
+    source_set_idx = rule_set[3]
+
+    if (len(rule_set[2]) > 0) and (rule_set[2][0] == ch):
+        # this is a scan, flip that rule about and add it to the next
+        # group, having the offset of the originating rule
+        add_to_set(next_rule_set, rule_set[0], [ch], [], source_set_idx)
+# end scan_rule
+
+
+# based on our current rule set
+def predict_rule(sets, rule_set, offset, grammar):
+    # what can this rule go to
+    post = rule_set[2][0]
+
+    # add predictions for those rules
+    sequences = grammar[post]
+    for seq in sequences:
+        add_to_set(sets[offset], post, [], seq, offset)
+
+
+# The 'complete' is -1, any set
+# that contains it
+def contains_pass(rule):
+    for item in rule:
+        if (item[0] == -1) and (item[1] == [0]) and rule_finished(item):
+            return True
+    return False
+
+
+def earley_parse(word, grammar):
+    sets = []
+
+    for i in range(len(word) + 1):
+        sets.append([])
+
+    # -1 is my 'complete' state, 0 goes to it
+    add_to_set(sets[0], -1, [], [0], 0)
+
+    set_count = len(sets)
+    for set_idx in range(set_count):
+        rule_sets = sets[set_idx]
+        rule_set_idx = 0
+        while rule_set_idx < len(rule_sets):
+            rule_set = rule_sets[rule_set_idx]
+            if rule_finished(rule_set):
+                complete_rule(sets, rule_set, set_idx)
+            else:
+                next_char = rule_set[2][0]
+                if is_terminal(next_char):
+                    scan_rule(sets, rule_set, set_idx, word)
+                else:
+                    predict_rule(sets, rule_set, set_idx, grammar)
+            rule_set_idx += 1
+    # end for
+
+    # passes if the final set contains a rule pointing back to -1
+    return contains_pass(sets[len(word)])
+# end earley parse
+
+
+
+
+
+
 @time_func
 def part02():
-    pass
+    count = 0
+    for word in inputs:
+        if earley_parse(word, rules):
+            count += 1
+
+    print(f'part02 answer: {count}')
 # end part02
 
 
 # run the parts
-part01()
+# part01()
 part02()
 
