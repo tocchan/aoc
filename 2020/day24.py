384d2bacf4ad141a595c53b293b05e57a6ec3178
Chris Forseth <christopher.forseth@gmail.com>
2020-12-23 23:03:13 -0600

    Stub out files;

---
+++ 2020/day24.py
@@ -0,0 +1,38 @@
+import io
+import re
+import sys
+import time
+from collections import deque
+from copy import deepcopy
+
+import aoc
+from utils import time_func
+
+# config
+INPUT_FILE = 'day24.input.txt'
+
+
+# read input
+file_text = str()
+with open(INPUT_FILE, 'r') as input_file:
+    file_text = input_file.read()
+
+
+# implementation
+@time_func
+def part01():
+    ans = 0
+    print(f'part01 answer: {ans}')
+# end part01
+
+
+@time_func
+def part02():
+    ans = 0
+    print(f'part02 answer: {ans}')
+# end part02
+
+
+# run the parts
+part01()
+part02()
